f1d064046d0535cab418bbe941020e2e63a00f39
Bruce Real <ggkids_bruce@yahoo.com>
2020-01-30 21:01:52 +0800

    a

--- controller/db.php
+++ controller/db.php
@@ -1,5 +1,7 @@
 <?php
 
+	header("Access-Control-Allow-Origin: *");
+
 	$localhost = 'localhost';
 	$user = 'root';
 	$pw = '';
--- stafflayouts/add_property_modal.php
+++ stafflayouts/add_property_modal.php
@@ -5,13 +5,14 @@
                 <h5 class="modal-title text-center">Property Details</h5>
                     <button type="button" class="close" data-dismiss="modal"><span>&times;</span></button>
             </div>
-            <form action="controller/properties_sale_data.php" method="POST">
                 <div class="modal-body">
                      <div class="row">
                        <div class="col">
                             <h5>Property Ownership</h5>
                        </div>
-                       <div class="col col-md-4">
+                    </div>
+                    <div class="row mt-3">
+                          <div class="col col-md-4">
                             <select name="category" id="category" class="form-control form-control-lg">
                                 <option value="">Please Select Category</option>
                                 <option value="Deed of Sale">Deed of Sale</option>
@@ -19,158 +20,9 @@
                             </select>
                        </div>
                     </div>
-                     <div class="row mt-4">
-                         <div class="col col-md-5">
-                            <div class="form-group">
-                                <label>First Name</label>
-                                <input type="text" class="form-control" name="fname">
-                            </div>
-                        </div>
-                        <div class="col col-md-5">
-                            <div class="form-group">
-                                <label>Last Name</label>
-                                <input type="text" class="form-control" name="lname">
-                            </div>
-                        </div>
-                        <div class="col col-md-2">
-                            <div class="form-group">
-                                <label>M.I</label>
-                                <input type="text" class="form-control" name="mi">
-                            </div>
-                        </div>
-                   </div>
-                   <div class="row">
-                        <div class="col">
-                            <div class="form-group">
-                                <label>Address</label>
-                                <input type="text" name="address" class="form-control">
-                            </div>
-                        </div>
-                   </div>
-                    <div class="row">
-                        <div class="col">
-                            <div class="form-group">
-                                <label>Barangay</label>
-                                <input type="text" name="brgy" class="form-control">
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="form-group">
-                                <label>District</label>
-                                <input type="text" name="district" class="form-control">
-                            </div>
-                        </div>
-                   </div>
-                    <div class="row">
-                        <div class="col">
-                            <div class="form-group">
-                                <label>City/Municipality</label>
-                                <input type="text" name="city" class="form-control">
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="form-group">
-                                <label>Province</label>
-                                <input type="text" name="province" class="form-control">
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="form-group">
-                                <label>Zip Code</label>
-                                <input type="text" name="zip" class="form-control">
-                            </div>
-                        </div>
-                   </div>
-                    <hr>
-                    <div class="row">
-                       <div class="col">
-                            <h5>Property Location</h5>
-                       </div>
-                    </div>
-                   <div class="row mt-4">
-                        <div class="col">
-                            <div class="form-group">
-                                <label>Barangay</label>
-                                <input type="text" class="form-control" name="brgy_loc">
-                            </div>
-                        </div>
-                   </div>
-                   <div class="row">
-                        <div class="col">
-                            <div class="form-group">
-                                <label>Property Location</label>
-                                <input type="text" class="form-control" name="location">
-                            </div>
-                        </div>
-                   </div>
-                   <div class="row">
-                        <div class="col">
-                            <div class="form-group">
-                                <label>Street/Extent</label>
-                                <input type="text" class="form-control" name="street">
-                            </div>
-                        </div>
-                   </div>
-                   <hr>
-                   <div class="row">
-                       <div class="col">
-                            <h5>Boundaries</h5>
-                       </div>
-                    </div>
-                    <div class="row mt-4">
-                        <div class="col">
-                            <div class="form-group">
-                                <label>North</label>
-                                <input type="text" name="north" class="form-control">
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="form-group">
-                                <label>South</label>
-                                <input type="text" name="south" class="form-control">
-                            </div>
-                        </div>
-                    </div>
-                     <div class="row">
-                        <div class="col">
-                            <div class="form-group">
-                                <label>East</label>
-                                <input type="text" name="east" class="form-control">
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="form-group">
-                                <label>West</label>
-                                <input type="text" name="west" class="form-control">
-                            </div>
-                        </div>
-                    </div>
-                    <hr>
-                    <div class="row">
-                       <div class="col">
-                            <h5>Property Price</h5>
-                       </div>
-                    </div>
-                    <div class="row mt-4">
-                       <div class="col">
-                            <div class="form-group">
-                                <label>Price</label>
-                                <input type="text" class="form-control" name="price">
-                            </div>
-                       </div>
-                        <div class="col">
-                            <div class="form-group">
-                                <label>Lot size</label>
-                                <input type="text" class="form-control" name="size">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-primary" name="submit">Save Property</button>
-                </div>
-            </form>
+              <?php include('property_sale.php'); ?>
+              <?php include('property_donation.php'); ?>
         </div>
     </div>
+  </div>
 </div>
